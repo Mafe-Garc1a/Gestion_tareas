e10282fb6d4afe7561286702d796247291a1f640
SantiOrBl <Santiago.orozco.bl@gmail.com>
2025-10-16 01:58:49 -0500

    Remove unused routers from main.py
    Removed unused routers for users and fincas.

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.router import users
-from app.router import fincas
 from app.router import metodo_pago
 from app.router import auth
 
 app = FastAPI()
 
 # Incluir en el objeto app los routers
-app.include_router(users.router, prefix="/users", tags=["users"])
+
 app.include_router(auth.router, prefix="/access", tags=["login"])
-app.include_router(fincas.router, prefix="/fincas", tags=["fincas"])
 app.include_router(metodo_pago.router, prefix="/metodo_pago", tags=["metodo_pago"])
 
 # Configuración de CORS para permitir todas las solicitudes desde cualquier origen
