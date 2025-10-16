2f725714fd7fc910a98653574a6d3e6861f07993
SantiOrBl <santiago.orozco.bl@gmail.com>
2025-10-16 00:24:37 -0500

    permisos v1

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.router import users
-from app.router import fincas
+from app.router import permisos
 from app.router import auth
 
 app = FastAPI()
 
 # Incluir en el objeto app los routers
-app.include_router(users.router, prefix="/users", tags=["users"])
+
 app.include_router(auth.router, prefix="/access", tags=["login"])
-app.include_router(fincas.router, prefix="/fincas", tags=["fincas"])
+app.include_router(permisos.router, prefix="/permisos", tags=["permisos"])
 
 # Configuración de CORS para permitir todas las solicitudes desde cualquier origen
 app.add_middleware(
